b31ee5c0b33749a33af63d4ceb640bdcda30cddd
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2022-09-20 07:59:02 -0700

    Build the CQM

--- tour_planning.py
+++ tour_planning.py
@@ -40,3 +40,28 @@ max_cost = sum(l["length"] for l in legs)*np.mean([c["Cost"] for c in transport.
 max_time = 0.5*sum(l["length"] for l in legs)/min(s["Speed"] for s in transport.values())
 
 t= [dimod.Binary(f'{mode}_{i}') for i in range(L) for mode in transport.keys()]
+
+def calculate_total(t, measure):
+    if measure == 'Exercise':
+        return dimod.quicksum(t[i]*transport[t[i].variables[0].split('_')[0]]['Exercise']*legs[i//num_modes]['length']*legs[i//num_modes]['uphill'] for i in range(num_modes*L))
+    elif measure == 'Time':
+        return dimod.quicksum(t[i]*legs[i//num_modes]['length']/transport[t[i].variables[0].split('_')[0]]['Speed'] for i in range(num_modes*L))
+    else:
+        return dimod.quicksum(t[i]*transport[t[i].variables[0].split('_')[0]][measure]*legs[i//num_modes]['length'] for i in range(num_modes*L))
+
+
+cqm = dimod.ConstrainedQuadraticModel()
+cqm.set_objective(-calculate_total(t, "Exercise"))
+
+for leg in range(L):
+    cqm.add_constraint(dimod.quicksum(t[num_modes*leg:num_modes*leg+num_modes]) == 1, label=f"One-hot leg{leg}")
+cqm.add_constraint(calculate_total(t, "Cost") <= max_cost, label="Total cost", weight=100, penalty='quadratic')
+cqm.add_constraint(calculate_total(t, "Time") <= max_time, label="Total time", weight=30, penalty='linear')
+
+drive_index = list(modes).index('drive')
+cycle_index = list(modes).index('cycle')
+for leg in range(L):
+     if legs[leg]['toll']:
+         cqm.add_constraint(t[num_modes*leg:num_modes*leg+num_modes][drive_index] == 0, label=f"Toll to drive on leg {leg}")
+     if legs[leg]['uphill'] > MAX_ELEVATION/2:
+         cqm.add_constraint(t[num_modes*leg:num_modes*leg+num_modes][cycle_index] == 0, label=f"Too steep to cycle on leg {leg}", weight=150)

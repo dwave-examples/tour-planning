599dcf0998f5ae1e7c3c61570d03403428a097f3
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2022-09-27 05:01:41 -0700

    Add solutions in human format

--- formatting.py
+++ formatting.py
@@ -15,6 +15,10 @@
 import pandas as pd
 import json
 
+__all__ = ["out_job_submit_state", "in_job_submit_state", "out_problem_human",
+    "out_problem_code", "in_problem_code", "out_inputs_human", "out_transport_human",
+    "out_solutions_human",]
+
 def out_job_submit_state(code):
     """Output status as 'Status: <status>'."""
     return f"Status: {code}"
@@ -28,6 +32,8 @@ def out_problem_human(problem):
     df = pd.DataFrame(problem)
     return df.to_string()
 
+out_transport_human = out_problem_human
+
 def out_problem_code(problem):
     """Output problem for code."""
     return json.dumps(problem)
@@ -35,3 +41,23 @@ def out_problem_code(problem):
 def in_problem_code(code):
     """Input problem from code."""
     return json.loads(code)
+
+def out_inputs_human(params):
+    """Output input ranges for humans."""
+    df = pd.DataFrame(params)
+    df_t = df.T
+    df_t.columns = ["Min.", "Max.", "Current Value"]
+    return df_t.to_string()
+
+def out_solutions_human(sampleset):
+    """Output solutions for humans."""
+    s = ""
+    sampleset_feasible = sampleset.filter(lambda row: row.is_feasible)
+    first = sorted({int(key.split('_')[1]): key.split('_')[0] for key,val in \
+        sampleset_feasible.first.sample.items() if val==1.0}.items())
+    ratio = round(len(sampleset_feasible)/len(sampleset), 1)
+    s += "Feasible solutions: {:.1%} of {} samples.\n".format((ratio), len(sampleset))
+    s += f"Best solution with energy {round(sampleset_feasible.first.energy)} is:\n"
+    for leg in first:
+        s += f"{leg}\n"
+    return s

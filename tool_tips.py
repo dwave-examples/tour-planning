4dcc71118362b7263bd97d5ea4eb432bd2e919e3
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2022-09-29 19:16:22 -0700

    Add remaining tooltips

--- tool_tips.py
+++ tool_tips.py
@@ -16,4 +16,13 @@ tool_tips = {"num_legs": "Number of legs for the tour.",
     "max_leg_length": "Maximum length for a single leg.",
     "min_leg_length": "Minimum length for a single leg.",
     "max_leg_slope": "Maximum elevation for a single leg.",
-    }
+    "max_cost": "Maximum you wish to pay for the tour.",
+    "max_time": "Maximum time you wish the entire tour to last.",
+    "weight_cost_input": "Weight you assign to the constraint on highest cost.",
+    "weight_cost_slider": "Weight you assign to the constraint on highest cost.",
+    "weight_time_input": "Weight you assign to the constraint on longest tour duration.",
+    "weight_time_slider": "Weight you assign to the constraint on longest tour duration.",
+    "weight_slope_input": "Weight you assign to the constraint on steepest leg to cycle.",
+    "weight_slope_slider": "Weight you assign to the constraint on steepest leg to cycle.",
+    "btn_solve_cqm": "Click to submit your problem to a Leap quantum-classical hybrid CQM solver."
+}

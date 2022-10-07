cb97aea23245008d318551e3653634e77f669437
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2022-10-07 10:31:21 -0700

    Tes legs() callback & update test_no_solver()

--- tests/test_no_solver.py
+++ tests/test_no_solver.py
@@ -12,8 +12,6 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-import os
-import sys
 from parameterized import parameterized
 import pytest
 
@@ -21,39 +19,24 @@ from contextvars import copy_context, ContextVar
 from dash._callback_context import context_value
 from dash._utils import AttributeDict
 
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.append(os.path.abspath('../'))
-
 btn_solve_cqm = ContextVar('btn_solve_cqm')
 
 import app
 
 @pytest.mark.parametrize("input_val, output_val",
-    [(0, True), (1, True)])
-def test_no_solver_no_solver(mocker, input_val, output_val):
-    mocker.patch.object(app, 'client', None)
+    [(0, True), (1, True), (0, False), (1, False)])
+def test_no_solver(mocker, input_val, output_val):
+
+    if output_val == True:
+        mocker.patch.object(app, 'client', None)
+    else:
+        mocker.patch.object(app, 'client', "Henry")
 
     def run_callback():
-            context_value.set(AttributeDict(**{"triggered_inputs":
-                [{"prop_id": "btn_solve_cqm.n_clicks"}]}))
-            return app.no_solver(btn_solve_cqm.get())
+        context_value.set(AttributeDict(**{"triggered_inputs":
+            [{"prop_id": "btn_solve_cqm.n_clicks"}]}))
 
-    btn_solve_cqm.set(input_val)
-
-    ctx = copy_context()
-
-    output = ctx.run(run_callback)
-    assert output == output_val
-
-@pytest.mark.parametrize("input_val, output_val",
-    [(0, False), (1, False)])
-def test_no_solver_with_solver(mocker, input_val, output_val):
-    mocker.patch.object(app, 'client', "Henry")
-
-    def run_callback():
-            context_value.set(AttributeDict(**{"triggered_inputs":
-                [{"prop_id": "btn_solve_cqm.n_clicks"}]}))
-            return app.no_solver(btn_solve_cqm.get())
+        return app.no_solver(btn_solve_cqm.get())
 
     btn_solve_cqm.set(input_val)
 

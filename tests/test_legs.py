cbfb639dc6a92fdc9b07402417ffa693bdeeda47
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2022-10-07 11:33:46 -0700

    Update code testing legs() callback

--- tests/test_legs.py
+++ tests/test_legs.py
@@ -12,8 +12,6 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-import os
-import sys
 from parameterized import parameterized
 import pytest
 
@@ -22,11 +20,7 @@ from dash._callback_context import context_value
 from dash._utils import AttributeDict
 from dash import no_update
 
-input_print = ContextVar("input_print")
-num_legs = ContextVar("num_legs")
-max_leg_length = ContextVar("max_leg_length")
-min_leg_length = ContextVar("min_leg_length")
-max_leg_slope = ContextVar("max_leg_slope")
+from formatting import tour_from_json
 
 in_print_generate_legs = """
 Configurable inputs have these supported ranges and current values:
@@ -51,36 +45,38 @@ weight_cost       0  100000           100 <<--
 output_placeholder = " "
 
 import app
-from formatting import tour_from_json
 
-@pytest.mark.parametrize("""
-input_print_val, num_legs_val, max_leg_length_val, min_leg_length_val,
-max_leg_slope_val, problem_print_code_val, problem_print_human_val
-""",
+input_print = ContextVar("input_print")
+for key in app.leg_inputs.keys():
+    vars()[key] = ContextVar(f"{key}")
+
+
+# num_legs = ContextVar("num_legs")
+# max_leg_length = ContextVar("max_leg_length")
+# min_leg_length = ContextVar("min_leg_length")
+# max_leg_slope = ContextVar("max_leg_slope")
+
+@pytest.mark.parametrize("input_print_val, " +
+    ",".join([f'{key}_val' for key in app.leg_inputs.keys()]) +
+    ", problem_print_code_val, problem_print_human_val",
     [(in_print_no_update, 10, 10, 3, 8, no_update, no_update),
     (in_print_generate_legs, 10, 10, 3, 8, output_placeholder, output_placeholder)])
 def test_legs(mocker, input_print_val, num_legs_val,
     max_leg_length_val, min_leg_length_val, max_leg_slope_val,
     problem_print_code_val, problem_print_human_val):
-    mocker.patch.object(app, "client", None)
 
     def run_callback():
         context_value.set(AttributeDict(
             **{
             "triggered_inputs": [{"prop_id": "input_print.value"}],
-            "state_values": [{"prop_id": "num_legs.value",
-	           "prop_id": "max_leg_length.value",
-	           "prop_id": "min_leg_length.value",
-	           "prop_id": "max_leg_slope.value"}]}))
+            "state_values": [{"prop_id": f"{key}.value"} for key in app.leg_inputs.keys()]}))
 
         return app.legs(input_print.get(), num_legs.get(), max_leg_length.get(),\
             min_leg_length.get(), max_leg_slope.get())
 
-    input_print.set(input_print_val)
-    num_legs.set(num_legs_val)
-    max_leg_length.set(max_leg_length_val)
-    min_leg_length.set(min_leg_length_val)
-    max_leg_slope.set(max_leg_slope_val)
+    input_print.set(vars()["input_print_val"])
+    for key in app.leg_inputs.keys():
+        globals()[key].set(vars()[key + "_val"])
 
     ctx = copy_context()
 

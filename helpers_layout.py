c066177c1e4b052cc7ebe7e66d4887deae2877b9
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2022-10-09 14:56:43 -0700

    Replace cool log sliders with penalty radio buttons (part 1)

--- helpers_layout.py
+++ helpers_layout.py
@@ -19,7 +19,7 @@ import numpy as np
 from tour_planning import leg_ranges, weight_ranges, budget_ranges
 from tour_planning import leg_init_values, weight_init_values, budget_init_values
 
-__all__ = ["_dcc_input", "_dcc_slider", "_dcc_radio"]
+__all__ = ["_dcc_input", "_dcc_slider", "_dcc_radio_penalty", "_dcc_radio_hardsoft"]
 
 ranges = {**leg_ranges, **weight_ranges, **budget_ranges}
 init_values = {**leg_init_values, **weight_init_values, **budget_init_values}
@@ -27,52 +27,46 @@ init_values = {**leg_init_values, **weight_init_values, **budget_init_values}
 def _dcc_input(name, step=None):
     """Construct ``dash.Input`` element for layout."""
 
-    suffix = ""
-    if "_slider" in name:
-        suffix = "_slider"
-        name = name.replace("_slider", "")
     return Input(
-        id=f"{name}{suffix}",
+        id=name,
         type="number",
         min=ranges[name][0],
         max=ranges[name][1],
         step=step,
         value=init_values[name])
 
-def _dcc_slider(name, step=1, discrete_slider=False):
+def _dcc_slider(name, step=1):
     """Construct ``dash.Slider`` elements for layout."""
 
-    suffix = ""
-    if "_slider" in name:
-        suffix = "_slider"
-        name = name.replace("_slider", "")
     max_range = ranges[f"{name}"][1]
     init_val = init_values[f"{name}"]
-    if not discrete_slider: # log slider
-        max_range = np.log10(max_range)
-        init_val = np.log10(init_val)
-        marks={ranges[f"{name}"][0]:
-                {"label": "Soft", "style": {"color": "white"}},
-            int(max_range):
-                {"label": "Softish", "style": {"color": "white"}}}
-    else:
-        marks={i: {"label": f"{str(i)}", "style": {"color": "white"}} for i in
-        range(ranges[name][0], ranges[name][1] + 1, 2*step)}
+    marks={i: {"label": f"{str(i)}", "style": {"color": "white"}} for i in
+    range(ranges[name][0], ranges[name][1] + 1, 2*step)}
 
     return Slider(
-        id=f"{name}{suffix}",
+        id=f"{name}",
         min=ranges[f"{name}"][0],
         max=max_range,
         marks=marks,
         step=step,
         value=init_val,)
 
-def _dcc_radio(name):
+def _dcc_radio_hardsoft(name):
     """Construct ``dash.RadioItem`` elements for layout."""
 
     return RadioItems([
         {"label": html.Div(['Soft   '], style={'color': 'white', 'font-size': 12}),
         "value": "soft",},
         {"label": html.Div(['   Hard'], style={'color': 'white', 'font-size': 12}),
-        "value": "hard",},], value='soft', id=f"{name}_radio",
+        "value": "hard",},], value='soft', id=f"{name}_hardsoft",
+        inputStyle={"margin-right": "20px"})
+
+def _dcc_radio_penalty(name):
+    """Construct ``dash.RadioItem`` elements for layout."""
+
+    return RadioItems([
+        {"label": html.Div(['Linear   '], style={'color': 'white', 'font-size': 12}),
+        "value": "linear",},
+        {"label": html.Div(['   Quadratic'], style={'color': 'white', 'font-size': 12}),
+        "value": "quadratic",},], value='quadratic', id=f"{name}_penalty",
         inputStyle={"margin-right": "20px"})

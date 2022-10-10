dd5b77c74e64aa473cee5a26deb7e1267367d4f3
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2022-10-10 09:19:04 -0700

    Add unittest for problem output in ``update_legs`` callback

--- tests/test_update_legs.py
+++ tests/test_update_legs.py
@@ -22,11 +22,9 @@ from dash import no_update
 
 import random
 
-output_placeholder = " "
-
 from app import names_leg_inputs, names_budget_inputs, names_weight_inputs
 from app import update_legs
-from formatting import tour_from_json
+from formatting import tour_from_json, tour_to_display
 
 changed_input = ContextVar("changed_input")
 for key in names_leg_inputs:
@@ -34,21 +32,17 @@ for key in names_leg_inputs:
 
 states = [{"prop_id": f"{key}.value"} for key in names_leg_inputs]
 
-output_placeholder = " "
-
 parametrize_names = "changed_input_val, " + \
     ", ".join([f'{key}_val' for key in names_leg_inputs]) + \
     ", problem_print_code_val, problem_print_human_val"
 
-parametrize_constant_vals = [(10, 10, 3, 8)]
-
 parametrize_vals = []
-for i in range(5):
+for i in range(10):
     trigger = random.choice([*names_leg_inputs, *names_budget_inputs,
         *names_weight_inputs])
-    output = output_placeholder if any(trigger == key for key in names_leg_inputs) else \
+    output = " " if any(trigger == key for key in names_leg_inputs) else \
         no_update
-    parametrize_vals.append((trigger, *parametrize_constant_vals[0], output, output))
+    parametrize_vals.append((trigger, 10, 10, 3, 8, output, output))
 
 @pytest.mark.parametrize(parametrize_names, parametrize_vals)
 def test_legs_generation_trigger(changed_input_val, num_legs_val,
@@ -77,3 +71,36 @@ def test_legs_generation_trigger(changed_input_val, num_legs_val,
         assert output == (no_update, no_update)
     else:
         assert len(tour_from_json(output[0])) == num_legs_val   # problem_print_code_val
+
+parametrize_names = "changed_input_val, " + \
+    ", ".join([f'{key}_val' for key in names_leg_inputs])
+parametrize_vals = [("num_legs", 1, 10, 10, 0)]
+
+@pytest.mark.parametrize(parametrize_names, parametrize_vals)
+def test_legs_output(changed_input_val, num_legs_val,
+    max_leg_length_val, min_leg_length_val, max_leg_slope_val):
+    """Test that output is correctly generated."""
+
+    def run_callback():
+        context_value.set(AttributeDict(
+            **{
+            "triggered_inputs": [{"prop_id": "num_legs.value"}],
+            "state_values": states}))
+
+        return update_legs(changed_input.get(), num_legs.get(), max_leg_length.get(),\
+            min_leg_length.get(), max_leg_slope.get())
+
+    changed_input.set(changed_input_val)
+    for key in names_leg_inputs:
+        globals()[key].set(vars()[f"{key}_val"])
+
+    ctx = copy_context()
+
+    output = ctx.run(run_callback)
+    problem_print_code_val = tour_from_json(output[0])
+    problem_print_human_val = output[1]
+
+    assert problem_print_code_val[0]["length"] == max_leg_length_val
+    assert problem_print_code_val[0]["uphill"] == max_leg_slope_val
+
+    assert problem_print_human_val == tour_to_display(problem_print_code_val)

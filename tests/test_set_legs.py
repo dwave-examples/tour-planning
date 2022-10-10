e17432393801d4f2c827b78326c6f240908e516a
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2022-10-10 08:15:26 -0700

    Add unittests to test_set_legs

--- tests/test_set_legs.py
+++ tests/test_set_legs.py
@@ -15,6 +15,7 @@
 from parameterized import parameterized
 import pytest
 
+import pandas as pd
 import random
 
 from app import names_leg_inputs
@@ -27,6 +28,8 @@ parametrize_vals = []
 for i in range(5):
     leg_vals = [random.randint(leg_ranges[key][0], leg_ranges[key][1]) for key
         in names_leg_inputs]
+    leg_vals = [leg_vals[0], max(leg_vals[0], leg_vals[1]),
+        min(leg_vals[0], leg_vals[1]), leg_vals[3]]
     parametrize_vals.append(tuple(leg_vals))
 
 @pytest.mark.parametrize(parametrize_names, parametrize_vals)
@@ -34,7 +37,14 @@ def test_set_legs(num_legs_val, max_leg_length_val,
     min_leg_length_val, max_leg_slope_val):
     """Test that legs are correctly generated."""
 
-    output = set_legs(num_legs_val, max_leg_length_val, min_leg_length_val,
+    output = set_legs(num_legs_val, min_leg_length_val, max_leg_length_val,
         max_leg_slope_val)
 
-    assert set(output[0].keys()) - set(["length", "uphill", "toll"]) == set()
+    df = pd.DataFrame(output)
+
+    assert df.shape == (num_legs_val, 3)
+    assert set(df.columns) - set(["length", "uphill", "toll"]) == set()
+    assert df["length"].sum() >= num_legs_val * min_leg_length_val
+    assert df["length"].sum() <= num_legs_val * max_leg_length_val
+    assert df["uphill"].max() <= max_leg_slope_val
+    set(df["toll"].unique()) == {False, True}
